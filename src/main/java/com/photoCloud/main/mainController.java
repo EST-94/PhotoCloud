0f09a8ee1eeba6706dd23acef44b670c28cb3e06
EST-94 <bxob1040@naver.com>
2022-06-22 23:07:19 +0900

    220622 fixed

--- src/main/java/com/photoCloud/main/mainController.java
+++ src/main/java/com/photoCloud/main/mainController.java
@@ -1,10 +1,14 @@
 package com.photoCloud.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,36 +21,50 @@ public class mainController {
 	@GetMapping("/test")
 	public String mainPage() {
 		
-		String resultString = "for testt";
+		String resultString = "for test connection with server";
 		
 		return resultString;
 	};
 	
 	// 1. DB에서 사용자에 관한 파라미터를 받아 테이블에 접근 및 경로를 불러옴
-	@GetMapping
+	@GetMapping("/initViewer")
 	public String resourceDirGetter() {
 		
-		return "1";
+		logger.info("/initViewer initiated.");
+		
+		String debugMod = "Y"; // 프로퍼티로 뺄 것
+		
+		if (debugMod.equals("Y")) {
+			
+			logger.debug("from /initViewer - debugmod initiated.");
+			Map<String, String> userInfoTestTemp = new HashMap<>();
+			
+			userInfoTestTemp.put("ID", "admin");
+			userInfoTestTemp.put("CDSID", "jhkim0001");
+			
+		} else {
+			
+		}
+		
+		
+		return "/paramSender.do";
 	};
 	
 	// 2. 자원경로를 받아 파라미터로 전달
-	@GetMapping()
+	@RequestMapping("/paramSender.do")
 	public String resourceParamSender() {
 		
-		return "1";
-	};
-	
-	
-	// 3. void-do 방식으로 자원경로를 받아 자원을 리턴함
-	@GetMapping("/sender.do")
-	public void photoSender() {
+		// 2-1. 자원경로 받아오는 서비스 구현
 		
+		
+		// 2-2. 받아온 자원경로를 통해 파일시스템 내의 파일 접근 후 리턴
+		
+		return "/doneViewer.do";
 	};
 	
 	
-	
-	// 4. 화면에 자원 출력
-	@GetMapping("/view")
+	// 3. 화면에 자원 출력
+	@RequestMapping("/doneViewer.do")
 	public String photoViewer(Model model) {
 		
 		logger.info("user accessed to /view.");
